63f401e660447cfff26aac05883aeaf20460a646
Nick Knudson <nickaknudson@gmail.com>
2013-02-28 10:13:21 -0500

    finished

--- solver.rb
+++ solver.rb
@@ -1,14 +1,62 @@
 #!/usr/bin/env ruby
 
+def depth(city, r, c)
+  while r < city.length && city[r][c].eql?('P')
+    r += 1
+  end
+  return r
+end
+
+def length(city, r, c)
+  while c < city[r].length && city[r][c].eql?('P')
+    c += 1
+  end
+  return c
+end
+
+def grow_point(city, r, c)
+  area = 0
+  for i in c...length(city, r, c) # exclusive range
+    l = i+1 - c
+    d = depth(city, r, i) - r
+    if l*d > area
+      area = l*d
+    end
+  end
+  return area
+end
+
+
+
+def analyze(city)
+  #analysis = Array.new
+  farea = 0
+  city.each_with_index do |row, r|
+    #arow = Array.new
+    row.each_with_index do |col, c|
+      if col.eql?('P')
+        area = grow_point city, r, c
+        #arow[c] = area
+        if area > farea
+          farea = area
+        end
+      end
+    #analysis[r] = arow
+    end
+  end
+  #return analysis
+  return farea
+end
+
 def load_city
   city = Array.new
   f = File.open("city_grid.txt", "r") #opens the file for reading
   f.each do |line| 
-    puts line
+    city.push(line.squeeze.split)
   end
   return city
 end
 
-
 puts "Loading city"
 city = load_city
+puts analyze city
